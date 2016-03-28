b1e528d60336f4766e71191c108e57c5d2140b26
nogaasha <sivosha2010@gmail.com>
2016-03-28 20:57:50 +0300

    add index.erb

---
+++ db/migrate/20160325173143_create_barbers.rb
@@ -0,0 +1,12 @@
+class CreateBarbers < ActiveRecord::Migration
+  def change
+  	 create_table :barbers do |t|
+  		t.text :name
+  		
+        t.timestamps
+  	end	
+  	Barber.create :name => 'Jessie Pinkman'
+  	Barber.create :name => 'Walter White'
+  	Barber.create :name => 'Gas Fring'
+  end
+end
