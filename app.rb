3dede59243192b2767f95592932ecd6031497d02
nogaasha <sivosha2010@gmail.com>
2016-03-30 22:45:17 +0300

    add validation lenth

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true,  length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -26,16 +26,18 @@ get '/' do
 	erb :index
 end
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		 erb "<h2>Спасибо, вы записались</h2>"
 	else
-	     erb "<h2>Ошибка</h2>"	 
+	     @error= @c.errors.full_messages.first
+	     erb :visit	 
 	end     
 
 	end
